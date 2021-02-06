a118806dc0ce1b4a5e91efc4ce2eaa08bed1f495
Baron <bb@gaudiolab.com>
2021-02-07 00:18:09 +0900

    initial commit

---
+++ app/src/main/java/com/newstone/soriwa/demo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.newstone.soriwa.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
