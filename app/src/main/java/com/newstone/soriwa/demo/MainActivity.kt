442ee3d0f27d8c65f1f2713d8e823b9215c43a75
Baron <bb@gaudiolab.com>
2021-03-10 01:13:38 +0900

    ref: connect library with app

--- app/src/main/java/com/newstone/soriwa/demo/MainActivity.kt
+++ app/src/main/java/com/newstone/soriwa/demo/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.newstone.soriwa.demo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+import com.newstone.soriwa.Soriwa
+
 class MainActivity : AppCompatActivity() {
+    lateinit var processor: Soriwa
+    lateinit var playBtn: Button
     
     override fun onStart() {
+        processor = Soriwa()
+        processor.init()
         super.onStart()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout)
+
+        playBtn = findViewById<Button>(R.id.playBtn)
+        playBtn.setOnClickListener { processor.play(0, 0) }
     }
 }
